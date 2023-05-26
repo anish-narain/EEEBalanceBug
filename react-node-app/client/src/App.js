b50144fd10297e9161b981b8fcee233c39a55b05
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-05-26 08:36:08 +0100

    netlify not working, reverted back to previous version

--- react-node-app/client/src/App.js
+++ react-node-app/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import "./App.css";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -34,23 +34,23 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <div className="App">
-        <p>This text is printed inside the App component</p>
-        <div className="input-container">
-          <input type="number" value={inputValue} onChange={handleInputChange} />
-        </div>
-        <div className="button-container">
-          <button onClick={() => handleButtonClick("Up")}>Up</button>
-          <button onClick={() => handleButtonClick("Down")}>Down</button>
-          <button onClick={() => handleButtonClick("Left")}>Left</button>
-          <button onClick={() => handleButtonClick("Right")}>Right</button>
-        </div>
+    <div className="App">
+      <p>This text is printed inside the App component</p>
+      <div className="input-container">
+        <input type="number" value={inputValue} onChange={handleInputChange} />
       </div>
-    </React.Fragment>
+      <div className="button-container">
+        <button onClick={() => handleButtonClick("Up")}>Up</button>
+        <button onClick={() => handleButtonClick("Down")}>Down</button>
+        <button onClick={() => handleButtonClick("Left")}>Left</button>
+        <button onClick={() => handleButtonClick("Right")}>Right</button>
+      </div>
+    </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
 export default App;
+
+
+
+
