a8d8e7c8da5ba79991800fca8758f8c815049698
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-05-26 08:16:01 +0100

    creating an index.html file

--- react-node-app/client/src/App.js
+++ react-node-app/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import ReactDOM from "react-dom";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -34,23 +34,23 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <p>This text is printed inside the App component</p>
-      <div className="input-container">
-        <input type="number" value={inputValue} onChange={handleInputChange} />
+    <React.Fragment>
+      <div className="App">
+        <p>This text is printed inside the App component</p>
+        <div className="input-container">
+          <input type="number" value={inputValue} onChange={handleInputChange} />
+        </div>
+        <div className="button-container">
+          <button onClick={() => handleButtonClick("Up")}>Up</button>
+          <button onClick={() => handleButtonClick("Down")}>Down</button>
+          <button onClick={() => handleButtonClick("Left")}>Left</button>
+          <button onClick={() => handleButtonClick("Right")}>Right</button>
+        </div>
       </div>
-      <div className="button-container">
-        <button onClick={() => handleButtonClick("Up")}>Up</button>
-        <button onClick={() => handleButtonClick("Down")}>Down</button>
-        <button onClick={() => handleButtonClick("Left")}>Left</button>
-        <button onClick={() => handleButtonClick("Right")}>Right</button>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
+ReactDOM.render(<App />, document.getElementById("root"));
+
 export default App;
-
-
-
-
